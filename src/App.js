86defb1a38097a6e6c38208b79e093ec2a6f4e15
Salem Abebe <salemabebe@yahoo.com>
2022-05-04 20:06:46 -0400

    create card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
+import Card from "./components/Card.js/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((data) => (
+        <Card
+          img={data.image}
+          characterName={data.characterName}
+          id={data.id}
+        />
+      ))}
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,62 +1,62 @@
 const data = [
   {
     id: 1,
-    image: `url({process.env.PUBLIC_URL+'/images/bison.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/bison.jpg`,
     characterName: "number1",
   },
   {
     id: 2,
-    image: `url({process.env.PUBLIC_URL+'/images/clownfish.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/clownfish.jpg`,
     characterName: "number2",
   },
   {
     id: 3,
-    image: `url({process.env.PUBLIC_URL+'/images/jaguar.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/jaguar.jpg`,
     characterName: "number3",
   },
   {
     id: 4,
-    image: `url({process.env.PUBLIC_URL+'/images/killerwhale.jpg'})`,
-    characterName: "number1",
+    image: process.env.PUBLIC_URL + `/images/killerwhale.jpg`,
+    characterName: "number4",
   },
   {
     id: 5,
-    image: `url({process.env.PUBLIC_URL+'/images/koala.jpg'})`,
-    characterName: "number1",
+    image: process.env.PUBLIC_URL + `/images/koala.jpg`,
+    characterName: "number5",
   },
   {
     id: 6,
-    image: `url({process.env.PUBLIC_URL+'/images/monarchbird.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/monarchbird.jpg`,
     characterName: "number6",
   },
   {
     id: 7,
-    image: `url({process.env.PUBLIC_URL+'/images/panda.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/panda.jpg`,
     characterName: "number7",
   },
   {
     id: 8,
-    image: `url({process.env.PUBLIC_URL+'/images/plover.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/plover.jpg`,
     characterName: "number8",
   },
   {
     id: 9,
-    image: `url({process.env.PUBLIC_URL+'/images/polarBear.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/polarBear.jpg`,
     characterName: "number9",
   },
   {
     id: 10,
-    image: `url({process.env.PUBLIC_URL+'/images/seaTurtle.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/seaTurtle.jpg`,
     characterName: "number10",
   },
   {
     id: 11,
-    image: `url({process.env.PUBLIC_URL+'/images/tiger.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/tiger.jpg`,
     characterName: "number11",
   },
   {
     id: 12,
-    image: `url({process.env.PUBLIC_URL+'/images/toucan.jpg'})`,
+    image: process.env.PUBLIC_URL + `/images/toucan.jpg`,
     characterName: "number12",
   },
 ];
